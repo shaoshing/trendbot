0675d1134a0fdb4af642b728933cfc59f8f25af5
sayali10 <sayali10@gmail.com>
2014-03-11 22:36:32 -0400

    to remove titles which are not in English

---
+++ DataPreprocessing/keep_en.sh
@@ -0,0 +1,27 @@
+#!/bin/ksh
+
+cat pagecounts-20140101-000000 | grep ^en\ >> pagecounts-20140101-000000_en
+cat pagecounts-20140101-000000 | grep ^en\ >> pagecounts-20140101-010000_en
+cat pagecounts-20140101-010000 | grep ^en\ >> pagecounts-20140101-020000_en
+cat pagecounts-20140101-020000 | grep ^en\ >> pagecounts-20140101-030000_en
+cat pagecounts-20140101-030000 | grep ^en\ >> pagecounts-20140101-040000_en
+cat pagecounts-20140101-040000 | grep ^en\ >> pagecounts-20140101-050000_en
+cat pagecounts-20140101-050000 | grep ^en\ >> pagecounts-20140101-060000_en
+cat pagecounts-20140101-060000 | grep ^en\ >> pagecounts-20140101-070000_en
+cat pagecounts-20140101-070000 | grep ^en\ >> pagecounts-20140101-080000_en
+cat pagecounts-20140101-070000 | grep ^en\ >> pagecounts-20140101-090000_en
+cat pagecounts-20140101-070000 | grep ^en\ >> pagecounts-20140101-100000_en
+cat pagecounts-20140101-100000 | grep ^en\ >> pagecounts-20140101-110000_en
+cat pagecounts-20140101-110000 | grep ^en\ >> pagecounts-20140101-120000_en
+cat pagecounts-20140101-120000 | grep ^en\ >> pagecounts-20140101-130000_en
+cat pagecounts-20140101-130000 | grep ^en\ >> pagecounts-20140101-140000_en
+cat pagecounts-20140101-140000 | grep ^en\ >> pagecounts-20140101-150000_en
+cat pagecounts-20140101-150000 | grep ^en\ >> pagecounts-20140101-160000_en
+cat pagecounts-20140101-160000 | grep ^en\ >> pagecounts-20140101-170000_en
+cat pagecounts-20140101-170000 | grep ^en\ >> pagecounts-20140101-180000_en
+cat pagecounts-20140101-180000 | grep ^en\ >> pagecounts-20140101-190000_en
+cat pagecounts-20140101-190001 | grep ^en\ >> pagecounts-20140101-200000_en
+cat pagecounts-20140101-200000 | grep ^en\ >> pagecounts-20140101-210000_en
+cat pagecounts-20140101-210000 | grep ^en\ >> pagecounts-20140101-220000_en
+cat pagecounts-20140101-220000 | grep ^en\ >> pagecounts-20140101-230000_en
+
