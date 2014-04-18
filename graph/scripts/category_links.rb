c6136b2f72264e72954a48545cb7058e2ef57ced
Shaoshing <shaoshing@me.com>
2014-04-17 21:42:25 -0400

    [graph] Update category-links script to generate links by category levels.

--- graph/scripts/category_links.rb
+++ graph/scripts/category_links.rb
@@ -1,35 +1,40 @@
+# Encoding: utf-8
+
 require 'rubygems'
 require 'neography'
 require 'mysql2'
 require 'json'
 
-PROCESS_MAIN_CATEGORY = false
+CATEGORY_LEVEL = 2
 
 db_configs = JSON.parse(File.read 'graph/configs/database.json')
 mysql_client = Mysql2::Client.new(
-  host: db_configs['host'],
-  username: db_configs['username'],
-  password: db_configs['password'],
-  port: db_configs['port'],
-  database: db_configs['database']
+  host: 'localhost',
+  username: 'root',
+  database: 'wiki-langs'
 )
 
-main_category_ids = File.read('graph/data/main_categories.txt').split("\n").join(', ')
-SQL_TEMPLATE = "
-  SELECT {select}
-  FROM page_categories AS pc1
-    JOIN page_categories AS pc2
-      ON pc1.pages = pc2.pages
-          AND pc1.id {main_categories} IN (#{main_category_ids})
-      JOIN Page AS p ON pc2.id = p.id
-      JOIN Category AS c ON pc2.pages = c.id
-  WHERE pc1.id = {page_id} AND pc2.id != {page_id}
-"
+category_title_map_id = {}
+category_titles = []
+File.read("graph/data/level#{CATEGORY_LEVEL}_categories.txt").lines.each do |line|
+  id, title = line.strip.split(", ", 2)
+  category_titles << title
+  category_title_map_id[title] = id
+end
 
-def make_sql(select, only_main_categories, page_id, limit = nil)
+SQL_TEMPLATE = %[
+  SELECT {select}
+  FROM categorylinks AS cl
+  JOIN categorylinks AS cl2 ON cl.cl_to = cl2.cl_to AND cl2.cl_type = "page"
+  JOIN page AS p ON cl2.cl_from = p.page_id
+  WHERE cl.cl_to IN ("#{category_titles.join('", "')}") AND
+    cl.cl_from = {page_id} AND
+    cl.cl_type = "page"
+]
+
+def make_sql(select, page_id, limit = nil)
   sql = SQL_TEMPLATE.dup
   sql.gsub!('{select}', select)
-  sql.gsub!('{main_categories}', only_main_categories ? '' : 'NOT')
   sql.gsub!('{page_id}', page_id)
   sql += " LIMIT #{limit}" if limit
   sql
@@ -44,10 +49,10 @@ start_page_index = 0
 (start_page_index...pages.size).each do |index|
   line = pages[index]
   level1_id, level1_title = line.strip.split(', ', 2)
-  puts "#{index + 1}. Processing #{PROCESS_MAIN_CATEGORY ? 'main' : 'sub' } category of [#{level1_id}, #{level1_title}]"
+  puts "#{index + 1}. Processing level #{CATEGORY_LEVEL} category of page [#{level1_id}, #{level1_title}]"
 
   links_count = mysql_client.query(
-      make_sql('COUNT(*)', PROCESS_MAIN_CATEGORY, level1_id)).first['COUNT(*)'].to_i
+      make_sql('COUNT(*)', level1_id)).first['COUNT(*)'].to_i
   batch_size = 100
   processed_links_count = 0
 
@@ -55,32 +60,42 @@ start_page_index = 0
     puts "-- processing #{processed_links_count} of #{links_count}"
     links = mysql_client.query(
         make_sql(
-          'p.id as page_id, p.name as page_title, c.id as category_id, c.name as category_title ',
-          PROCESS_MAIN_CATEGORY, level1_id, "#{processed_links_count}, #{batch_size}"
+          'p.page_id AS page_id, p.page_title AS page_title, cl2.cl_to AS category_title',
+          level1_id, "#{processed_links_count}, #{batch_size}"
         )
     )
     processed_links_count += batch_size
 
     links.each do |link|
+      link['page_title'] = link['page_title'].force_encoding('UTF-8')
+      link['category_title'] = link['category_title'].force_encoding('UTF-8')
+
       unless created_ids_cache[link['page_id'].to_s]
         neo4j_client.execute_query("
-            MERGE (p:Page {id: #{link['page_id']}, title: \"#{mysql_client.escape(link['page_title'])}\"})")
+            MERGE (p:Page {
+              id: #{link['page_id']},
+              title: \"#{mysql_client.escape(link['page_title'])}\"
+            })")
         created_ids_cache[link['page_id'].to_s] = true
       end
 
-      unless created_ids_cache[link['category_id'].to_s]
+      unless created_ids_cache[link['category_title'].to_s]
         neo4j_client.execute_query("
-            MERGE (p:Category {id: #{link['category_id']}, title: \"#{mysql_client.escape(link['category_title'])}\", main: true})")
-        created_ids_cache[link['category_id'].to_s] = true
+            MERGE (p:Category {
+              id: #{category_title_map_id[link['category_title']]},
+              title: \"#{mysql_client.escape(link['category_title'])}\",
+              level: #{CATEGORY_LEVEL}
+            })")
+        created_ids_cache[link['category_title'].to_s] = true
       end
 
-      key = link['page_id'].to_s + '-' + level1_id.to_s + '-' + link['category_id'].to_s
+      key = link['page_id'].to_s + '-' + level1_id.to_s + '-' + link['category_title'].to_s
       unless created_ids_cache[key]
         neo4j_client.execute_query("
             MATCH
               (p:Page {id: #{link['page_id']}}),
               (l:Page {id: #{level1_id}}),
-              (c:Category {id: #{link['category_id']}})
+              (c:Category {id: #{category_title_map_id[link['category_title']]}})
             MERGE p -[:BELONGS_TO]-> c
             MERGE c <-[:L1_BELONGS_TO]- l")
         created_ids_cache[key] = true
